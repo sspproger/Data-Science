9b94b013b6facc94f9d9dcc09e13fad36f76ed2f
Anton Pershin <ant.pershin2017@yandex.ru>
2026-02-27 11:30:05 +0300

    ex05

--- src/ex05/generator.py
+++ src/ex05/generator.py
@@ -1,6 +1,5 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 import sys
-import time
 import psutil
 
 process = psutil.Process()
@@ -10,13 +9,19 @@ def read_lines(filename):
         for line in f:
             yield line
 
-start = time.time()
-for line in read_lines(sys.argv[1]):
-    pass
-end = time.time()
+def main():
 
-memory = process.memory_info().rss / (1024**3)
-cpu = process.cpu_times().user + process.cpu_times().system
+    if len(sys.argv) != 2:
+        sys.exit(1)
 
-print(f"Peak Memory Usage = {memory:.3f} GB")
-print(f"User Mode Time + System Mode Time = {cpu:.2f}s")
+    for line in read_lines(sys.argv[1]):
+        pass
+
+    memory = process.memory_info().rss / (1024**3)
+    cpu = process.cpu_times().user + process.cpu_times().system
+
+    print(f"Peak Memory Usage = {memory:.3f} GB")
+    print(f"User Mode Time + System Mode Time = {cpu:.2f}s")
+
+if __name__ == '__main__':
+    main()
--- src/ex05/ordinary.py
+++ src/ex05/ordinary.py
@@ -1,19 +1,25 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 import sys
-import time
 import psutil
 
 process = psutil.Process()
-with open(sys.argv[1]) as f:
-    lines = f.readlines()
 
-start = time.time()
-for line in lines:
-    pass
-end = time.time()
+def main():
 
-memory = process.memory_info().rss / (1024**3)  # в GB
-cpu = process.cpu_times().user + process.cpu_times().system
+    if len(sys.argv) != 2:
+        sys.exit(1)
+        
+    with open(sys.argv[1]) as f:
+        lines = f.readlines()
 
-print(f"Peak Memory Usage = {memory:.3f} GB")
-print(f"User Mode Time + System Mode Time = {cpu:.2f}s")
+    for line in lines:
+        pass
+
+    memory = process.memory_info().rss / (1024**3) 
+    cpu = process.cpu_times().user + process.cpu_times().system
+
+    print(f"Peak Memory Usage = {memory:.3f} GB")
+    print(f"User Mode Time + System Mode Time = {cpu:.2f}s")
+
+if __name__ == '__main__':
+    main()    
